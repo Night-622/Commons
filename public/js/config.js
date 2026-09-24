0fb183d1b16e961202f858f8d80929d07de452de
Night <george@katris.net>
2026-09-25 09:42:20 +1000

    Initial commit

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,11 @@
-// Paste your web app config from Firebase console > Project settings > Your apps.
+// Firebase web config for the commons-ww project.
 // This config is public by design; Firestore security rules are what protect the data.
 export const firebaseConfig = {
-  apiKey: 'REPLACE_ME',
-  authDomain: 'REPLACE_ME.firebaseapp.com',
-  projectId: 'REPLACE_ME',
-  storageBucket: 'REPLACE_ME.appspot.com',
-  messagingSenderId: 'REPLACE_ME',
-  appId: 'REPLACE_ME',
+  apiKey: 'AIzaSyCY1GuPFAeiel-cq8zEyZ3W61nt4qZEcX8',
+  authDomain: 'commons-ww.firebaseapp.com',
+  projectId: 'commons-ww',
+  storageBucket: 'commons-ww.firebasestorage.app',
+  messagingSenderId: '899430592169',
+  appId: '1:899430592169:web:f21a6d329a063754d103b8',
+  measurementId: 'G-60D3W33DSW',
 };
